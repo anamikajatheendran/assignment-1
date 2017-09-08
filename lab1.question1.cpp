42b91ea731cf9c74afe8ffbec885de9a77dde687
anamikajatheendran <31594761+anamikajatheendran@users.noreply.github.com>
2017-09-08 21:42:08 +0530

    Add files via upload

---
+++ lab1.question1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int number=88;
+float decimal=22.3;
+bool statement=true;
+char charecter='p';
+cout<<number<<endl<<decimal<<endl<<statement<<endl<<charecter;
+return 0;
+}
